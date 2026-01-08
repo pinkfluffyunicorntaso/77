9411f8dbb0fb4d23c5f4a8755f2ed6947fb9658f
pinkfluffyunicorntaso <anastasia.kvachantiradze18@gmail.com>
2026-01-08 05:20:31 +0400

    hiiiiiiiiiiiiiiiiiiiii （￣︶￣

---
+++ Day 76/Classwork/script.js
@@ -0,0 +1,23 @@
+console.log(Math.PI)
+
+console.log(Math.random() * 1000)
+
+let myname = "taso"
+
+var myage = 13
+
+const mygroup = 77
+
+console.log(`hellou, my name is ${myname} i am ${myage} years old and i study in group ${mygroup}`)
+
+
+let num1 = Math.random() * 10
+let num2 = Math.random() * 30
+
+console.log(num1 ** num2)
+
+let text = "str"
+let num = 1
+let t_f = true
+
+console.log(typeof text, typeof num, typeof t_f)
